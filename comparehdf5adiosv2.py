c32fef6baa853b60b6839388b50f92a6f4dadcf0
manuel garcia <admin@chinchay.dev>
2024-02-22 20:05:42 -0300

    refactoring

--- comparehdf5adiosv2.py
+++ comparehdf5adiosv2.py
@@ -13,7 +13,7 @@ def write_hdf5(dataset):
 
 def write_adios(dataset):
     adios = adios2.Adios
-    io = adios.declare_io(__name__)
+    io = adios2.declare_io("myio")
     writer = io.Open('test_adios2.bp', adios2.Mode.Write)
     writer.Put('my_dataset', dataset)
     writer.Close()
@@ -25,7 +25,8 @@ def read_hdf5():
 
 def read_adios():
     adios = adios2.Adios
-    io = adios.declare_io(__name__)
+    myio = "readio"
+    io = adios2.declare_io("myio")
     reader = io.Open('test_adios2.bp', adios2.Mode.Read)
     variable = io.InquireVariable('my_dataset')
     shape = variable.Shape()
