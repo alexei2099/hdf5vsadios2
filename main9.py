9498bb0d8b286a98da998f03e1e470477145d5dc
manuel garcia <admin@chinchay.dev>
2024-02-23 00:37:54 -0300

    refactoring

--- main9.py
+++ main9.py
@@ -8,27 +8,22 @@ def generate_dataset():
     return np.random.rand(num_elements).astype(np.float64)
 
 def write_adios(dataset, filename='dataset.bp'):
-    shape = dataset.shape
     start_time = time.time()
     with adios2.open(filename, 'w') as fw:
-        var = fw.define_variable('my_dataset', shape=shape, dtype='double')
         fw.write('my_dataset', dataset)
     write_time = time.time() - start_time
     print(f"Tiempo de escritura con ADIOS2: {write_time} segundos.")
     return write_time
 
 def read_adios(filename='dataset.bp'):
+    dataset_read = None
     start_time = time.time()
     with adios2.open(filename, 'r') as fr:
         for step in fr:
-            # Asumiendo que 'my_dataset' es el único dato o el primero
-            vars_info = step.available_variables()
-            var_info = vars_info['my_dataset']
-            shape = tuple(map(int, var_info['Shape'].split(',')))
             dataset_read = step.read('my_dataset')
     read_time = time.time() - start_time
     print(f"Tiempo de lectura con ADIOS2: {read_time} segundos.")
-    return np.array(dataset_read).reshape(shape), read_time
+    return dataset_read, read_time
 
 if __name__ == "__main__":
     dataset = generate_dataset()
